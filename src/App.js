caad386ed9ce48c4d9f28e05f27424eb6db8f170
Ahmed <you@example.com>
2021-06-17 03:02:22 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Input from './components/Input'
+import List from './components/List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    items: [],
+    item: '',
+    adding: true,
+    editing: false,
+    deleting: false,
+    deletingAll: false,
+  }
+
+  handleTyping(e) {
+    console.log('type')
+  }
+  handleSubmit(id = '') {
+    console.log('submit')
+  }
+  addItem() {
+    console.log('add')
+  }
+  editItem(id) {
+    console.log('edit')
+  }
+  deleteItem(id) {
+    console.log('delete')
+  }
+  deleteAll(id) {
+    console.log('delete all')
+  }
+
+  resetStates() {
+    // const adding = !this.state.adding
+    // const editing = !this.state.editing
+    // const deleting = !this.state.deleting
+    // const deletingAll = !this.state.deletingAll
+    // this.setState({ ...adding, editing, deleting, deletingAll })
+    console.log(this.state)
+  }
+  render() {
+    console.log(this.state)
+
+    return (
+      <div>
+        <h1 onClick={this.resetStates}>App</h1>
+        <Input
+          handleTyping={this.handleTyping}
+          handleSubmit={this.handleSubmit}
+        />
+        <List />
+      </div>
+    )
+  }
 }
-
-export default App;
